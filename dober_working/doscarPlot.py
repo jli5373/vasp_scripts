dbd7a679daa5942ae8d254bf35916bf227dfcbcf
Derick Ober <derickober@ResNet-54-168.resnet.ucsb.edu>
2021-04-20 16:09:05 -0700

    made a vasp library module, added poscar class to the module (in dober_working)

--- dober_working/doscarPlot.py
+++ dober_working/doscarPlot.py
@@ -4,16 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import os
-import seaborn as sns
 
 
 #User input:
 spin_polarized = False
 singleElementDos = False             #True will selectively plot the DOS for only one element
-combineLikeOrbitals = True              #True will sum px,py,pz, same for d
 atomOfInterest = 'Zr'               #Only plot the DOS for this element
 normalizeAtom = 'Zr'                #Normalize by the number of these atoms (typically the metal)
-customEnergyDomain = [-9,19]         #Set to False for automatic plot scaling
+customEnergyDomain = [-11,30]         #Set to False for automatic plot scaling
 customDosRange = [0,3]               #set to False for automaic plot scaling
 
 #provide path to the DOSCAR simulation directory
@@ -22,7 +20,7 @@ doscar = os.path.join(simulationDir, 'DOSCAR')
 poscar = os.path.join(simulationDir, 'POSCAR')
 print('Spin polarized == %s' % str(spin_polarized))
 print('Single Element Dos == %s' % str(singleElementDos))
-print('Atom of Interest is %s\n' % atomOfInterest)
+print('Atom of Interest is %s' % atomOfInterest)
 
 
 #Read in the element types and number of each element from the poscar, store in a dictionary
@@ -40,7 +38,6 @@ with open(poscar) as infile:
             lineCount += 1
     infile.close()
 atomNumberLookup = dict(zip(identities, numberOfAtoms))
-print(atomNumberLookup[normalizeAtom])
 
 
 #Get doscar parameters: E(max), E(min), (the energy range in which the DOS is given), NEDOS,  E(fermi), 1.0000
@@ -48,7 +45,7 @@ print(atomNumberLookup[normalizeAtom])
 atomCount = 0       #keep track of which atom the file read is on. 0 corresponds to the general DOS
 lineCount = 0
 fullDos = []
-atomDos = []        #holds full dos data matrix for each atom: array of matrices, one matrix per atom
+atomDos = []
 doscarParams = None
 with open(doscar) as f:
     for line in f:
@@ -119,85 +116,71 @@ else:
     combinedSpinsDos = projectedDos / atomNumberLookup[normalizeAtom]
 
 
-
-
-
 #do a breakdown by element ID
 startIndex = 0
 atomDosArray = np.array(atomDos)
 elementBreakdownDos = []
-elementBreakdownEnergies = atomDosArray[0][:,0]
-numberOfOrbitals = []
-#Iterating through "i" different elements in the poscar
+elementBreakdownEnergies = np.zeros(len(atomDosArray[0]))
 for i in range(len(numberOfAtoms)):
-    collectElementDos = sum(atomDosArray[startIndex:startIndex+atomNumberLookup[identities[i]]]) / atomNumberLookup[normalizeAtom]              #sum matrices of like atoms       
-    
-
-    if combineLikeOrbitals:
-        combineS = np.reshape(np.array(collectElementDos[:,1]), (-1,1))
-        combineP = np.reshape(np.sum(np.array(collectElementDos[:,2:5]), axis=1), (-1,1))
-        combineD = np.reshape(np.sum(np.array(collectElementDos[:,5:]), axis=1), (-1,1))
-
-        collectElementDos = np.concatenate((np.reshape(elementBreakdownEnergies,(-1,1) ), combineS, combineP, combineD ), axis=1)
-        print(np.shape(collectElementDos))
-
-    #VERY TEMPORARY FIX:
-    #future fix: if a type of orbital is completely empty, remove it
-    if identities[i] == 'N':
-        if combineLikeOrbitals:
-            collectElementDos = collectElementDos[:,0:3]
-        else:
-            collectElementDos = collectElementDos[:,0:5]
-    
-    elementBreakdownDos.append(collectElementDos[:,1:])                                                                                         #append summed matrix for element "i", excluding the summed energies
-    numberOfOrbitals.append(len(collectElementDos[0,1:]))                       #count the number of different occupied orbitals for species i (EX: if Nitrogen, there is s,px,py,pz, no d: there are 4 occupied orbitals)
-    print(np.sum(collectElementDos, axis=0))
+    collectElementDos = sum(atomDosArray[startIndex:startIndex+atomNumberLookup[identities[i]]]) / atomNumberLookup[normalizeAtom]
+    elementBreakdownEnergies = elementBreakdownEnergies + collectElementDos[:,0]
+    elementBreakdownDos.append(np.sum(collectElementDos[:,1:], axis=1))
     startIndex = startIndex + atomNumberLookup[identities[i]]
 
-#stack up orbital DOS for different atoms on top of each other (append matrix of orbital DOS to the right of an existing DOS matrix- axis 1 is append horizontal)
-elementBreakdownPlot = np.array(elementBreakdownDos[0])
+elementBreakdownPlot = np.array(elementBreakdownDos[0]).reshape(-1,1)
 for j in range(1,len(elementBreakdownDos)):
-    elementBreakdownPlot = np.append(elementBreakdownPlot, np.array(elementBreakdownDos[j]), axis=1)
+    elementBreakdownPlot = np.append(elementBreakdownPlot, np.array(elementBreakdownDos[j]).reshape(-1,1), axis=1)
 
 
-
-
-
-#plot stacks by element and orbital
-#generate labels for the stack plot
-labels = []
-if combineLikeOrbitals:
-    orbitals = ['s', 'p', 'd']
-else:
-    orbitals = ['s(u,d)' , 'p_y(u,d)', 'p_z(u,d)', 'p_x(u,d)', 'd_[xy](u,d)', 'd_[yz](u,d)','d_[z2-r2](u,d)', 'd_[xz](u,d)', 'd_[x2-y2](u,d)]']
-
-for i in range(len(numberOfAtoms)):
-    for j in range(numberOfOrbitals[i]):
-        labels.append('%s %s ' % (identities[i], orbitals[j]) )
+#plot stacks by element
+plt.subplot(1,2,1)
 a = elementBreakdownEnergies
 b = elementBreakdownPlot.transpose()
-
-if combineLikeOrbitals:
-    plt.stackplot(a,b, colors=sns.color_palette('colorblind'))
-else:
-    plt.stackplot(a,b, colors=sns.color_palette('hls', sum(numberOfOrbitals)+1))
-#plt.plot(a,simpleCombinedUpDownDos, color='b')
+plt.stackplot(a,b)
 plt.vlines(doscarParams[-2],min(simpleCombinedUpDownDos), max(simpleCombinedUpDownDos), linestyles='dashed', color='k')
-plt.legend(labels)
+plt.legend(identities)
 plt.xlabel('eV', fontsize=18)
-plt.ylabel('DOS (Per %s)'%(normalizeAtom), fontsize=18)
-plt.title('Breakdown by Element and Orbital', fontsize=20)
+plt.ylabel('DOS (eV / %s)'%(normalizeAtom), fontsize=18)
+plt.title('Breakdown by Element', fontsize=20)
+if customEnergyDomain:
+    plt.xlim(customEnergyDomain)
+if customDosRange:
+    plt.ylim(customDosRange)
+     
+
+'''
+plt.subplot(2,1,1)
+plt.plot(fullDos[:,0], simpleCombinedUpDownDos)
+plt.ylabel('DOS per atom', fontsize=18)
+plt.title(doscar, fontsize=30)
+
+plt.subplot(2,1,2)
+'''
+
+
+#plot stacks by orbital
+plt.subplot(1,2,2)
+print(atomNumberLookup[normalizeAtom])
+x = combinedSpinsDos[:,0]
+y = combinedSpinsDos[:,1:].transpose()
+plt.stackplot(x,y)
+
+plt.vlines(doscarParams[-2],min(simpleCombinedUpDownDos), max(simpleCombinedUpDownDos), linestyles='dashed', color='k')
+plt.legend(['s(u,d)' , 'p_y(u,d)', 'p_z(u,d)', 'p_x(u,d)', 'd_[xy](u,d)', 'd_[yz](u,d)','d_[z2-r2](u,d)', 'd_[xz](u,d)', 'd_[x2-y2](u,d)]'], loc='upper left')
+plt.xlabel('eV', fontsize=18)
+
+caveat = ''
+if singleElementDos:
+    caveat = 'Only %s' % atomOfInterest
+else:
+    caveat = 'All atoms'
+plt.title('Breakdown by Orbital (%s)' % caveat, fontsize=20)
 if customEnergyDomain:
     plt.xlim(customEnergyDomain)
 if customDosRange:
     plt.ylim(customDosRange)
 
-#for debugging    
-#for i in elementBreakdownEnergies:
-#    print(i)
-print(doscarParams[-2])
-
 fig = plt.gcf()
 fig.set_size_inches(18.5, 10)
-fig.savefig(os.path.join(simulationDir, '%s_doscarPlot.pdf' % (simulationDir.split('/')[-4] + '_' + simulationDir.split('/')[-3] )), dpi=100)
+fig.savefig(os.path.join(simulationDir, 'doscarPlot.pdf'), dpi=100)
 plt.show()
--- plotBasisDeformation.py
+++ plotBasisDeformation.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 #provide full path for the read file
 deformFile = sys.argv[1]
 compositionFile = sys.argv[2]
-
+customTitle = 'ZrN Rocksalt Basis Deformation'                    #change to false for automatic title
 
 deformation = np.loadtxt(deformFile, usecols=2)
 deformationLabels = np.genfromtxt(deformFile, dtype='str', usecols=0)
@@ -26,7 +26,10 @@ for i in range(len(deformation)):
 plotData = de.columnSort(plotData, 0)
 
 plt.scatter(plotData[:,0], plotData[:,1], color='xkcd:crimson')
-plt.title(deformFile.split('/')[-1], fontsize=30)
+if customTitle:
+    plt.title(customTitle, fontsize=30)
+else:
+    plt.title(deformFile.split('/')[-1], fontsize=30)
 plt.xlabel('Composition [N/Zr]', fontsize=18)
-plt.ylabel('Deformation', fontsize=18)                  #units unknown, probably angstroms
+plt.ylabel('Mapping Score', fontsize=18)                  #units unknown, probably angstroms
 plt.show()
